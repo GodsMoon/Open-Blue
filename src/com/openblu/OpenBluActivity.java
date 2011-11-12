ab738046b1814bdf2784e0308852dd93282d024f
Sean Baskin <seanbaskin@gmail.com>
2011-11-12 12:17:08 -0500

    New project added

---
+++ src/com/openblu/OpenBluActivity.java
@@ -0,0 +1,13 @@
+package com.openblu;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class OpenBluActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
